d6833ee69cc61d8a050b8302767f28ea58f21374
takutyan318 <takuya.blackcat.318@gmail.com>
2022-07-15 15:57:15 +0900

    migrate,seed設定変更

---
+++ src/database/seeders/BooksTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //テーブルのクリア
+        DB::table('books')->truncate();
+
+        //初期データの用意
+        $books = [
+            ['name' => 'PHP Book',
+            'price' => 2000,
+            'author' => 'PHPER'],
+            ['name' => 'Laravel Book',
+            'price' => 3000,
+            'author' => null],
+            ['name' => 'Ruby Book',
+            'price' => 2500,
+            'author' => 'Rubyist']
+        ];
+
+        //登録
+        foreach ($books as $book) {
+            DB::table('books')->insert($book);
+        }
+    }
+}
