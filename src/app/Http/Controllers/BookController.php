07f30c7d2518aef3f357879143a439fcac5e2910
takutyan318 <takuya.blackcat.318@gmail.com>
2022-07-21 15:46:31 +0900

    更新・削除処理追加

--- src/app/Http/Controllers/BookController.php
+++ src/app/Http/Controllers/BookController.php
@@ -75,7 +75,13 @@ class BookController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $book = Book::findOrFail($id);
+        $book->name = $request->name;
+        $book->price = $request->price;
+        $book->author = $request->author;
+        $book->save();
+
+        return redirect('/book');
     }
 
     /**
@@ -86,6 +92,9 @@ class BookController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return redirect('/book');
     }
 }
