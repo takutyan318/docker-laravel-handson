f7ddf7595b9db3e256c0dc2a2256ecde855d1946
takutyan318 <takuya.blackcat.318@gmail.com>
2022-07-22 09:40:31 +0900

    バリデーションのエラーメッセージを変更

--- src/app/Http/Requests/BookRequest.php
+++ src/app/Http/Requests/BookRequest.php
@@ -19,4 +19,15 @@ class BookRequest extends FormRequest
             'author' => 'nullable|string|max:50'
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '書籍名を入力してください',
+            'name.string' => '書籍名は文字列で入力してください',
+            'name.max' => '書籍名は50文字以内で入力してください',
+            'price.required' => '値段を入力してください',
+            'price.integer' => '値段は数値で入力してください'
+        ];
+    }
 }
